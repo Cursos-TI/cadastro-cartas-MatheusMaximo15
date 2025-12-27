49b878c9586a19d5f837d6da6b9578379f2b4ef1
Matheus Máximo Linhares Maciel Silva <74806715+MatheusMaximo15@users.noreply.github.com>
2025-12-26 23:26:23 -0300

    Implementação de nivel Mestre
    Updated population variables to use unsigned long int and added super power calculations for each card.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,23 +11,25 @@ int main() {
   char estado1;
   char codigo1[4];
   char cidade1[50];
-  int populacao1;
+  unsigned long int populacao1;
   float area1;
   float pib1;
   int pontosTuristicos1;
   float densidadePopulacional1;
   float pibPerCapita1;
+  float superPoder1;
 
   // Declaração das variáveis da segunda carta
   char estado2;
   char codigo2[4];
   char cidade2[50];
-  int populacao2;
+  unsigned long int populacao2;
   float area2;
   float pib2;
   int pontosTuristicos2;
   float densidadePopulacional2;
   float pibPerCapita2;
+  float superPoder2;
 
   // Área para entrada de dados
   // Cadastro da primeira carta
@@ -39,7 +41,7 @@ int main() {
   printf("Digite o nome da cidade: ");
   scanf("%s", cidade1);
   printf("Digite a populacao: ");
-  scanf("%d", &populacao1);
+  scanf("%lu", &populacao1);
   printf("Digite a area em km2: ");
   scanf("%f", &area1);
   printf("Digite o PIB: ");
@@ -56,7 +58,7 @@ int main() {
   printf("Digite o nome da cidade: ");
   scanf("%s", cidade2);
   printf("Digite a populacao: ");
-  scanf("%d", &populacao2);
+  scanf("%lu", &populacao2);
   printf("Digite a area em km2: ");
   scanf("%f", &area2);
   printf("Digite o PIB: ");
@@ -65,12 +67,14 @@ int main() {
   scanf("%d", &pontosTuristicos2);
 
   // Calculos das propriedades da primeira carta
-  densidadePopulacional1 = populacao1 / area1;
+  densidadePopulacional1 = (float)populacao1 / area1;
   pibPerCapita1 = (pib1 * 1000000000) / populacao1;
+  superPoder1 = (float)populacao1 + area1 + pib1 + pontosTuristicos1 + pibPerCapita1 + (1.0 / densidadePopulacional1);
 
   // Calculos das propriedades da segunda carta
-  densidadePopulacional2 = populacao2 / area2;
+  densidadePopulacional2 = (float)populacao2 / area2;
   pibPerCapita2 = (pib2 * 1000000000) / populacao2;
+  superPoder2 = (float)populacao2 + area2 + pib2 + pontosTuristicos2 + pibPerCapita2 + (1.0 / densidadePopulacional2);
 
   // Área para exibição dos dados da cidade
   // Exibição da primeira carta
@@ -78,7 +82,7 @@ int main() {
   printf("Estado: %c\n", estado1);
   printf("Codigo da Carta: %s\n", codigo1);
   printf("Nome da Cidade: %s\n", cidade1);
-  printf("Populacao: %d\n", populacao1);
+  printf("Populacao: %lu\n", populacao1);
   printf("Area: %.2f km2\n", area1);
   printf("PIB: %.2f bilhoes\n", pib1);
   printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos1);
@@ -90,12 +94,22 @@ int main() {
   printf("Estado: %c\n", estado2);
   printf("Codigo da Carta: %s\n", codigo2);
   printf("Nome da Cidade: %s\n", cidade2);
-  printf("Populacao: %d\n", populacao2);
+  printf("Populacao: %lu\n", populacao2);
   printf("Area: %.2f km2\n", area2);
   printf("PIB: %.2f bilhoes\n", pib2);
   printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos2);
   printf("Densidade Populacional: %.2f hab/km2\n", densidadePopulacional2);
   printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
+
+  // Comparacao de cartas
+  printf("\nComparacao de Cartas:\n");
+  printf("Populacao: Carta %d venceu (%d)\n", (populacao1 > populacao2) ? 1 : 2, populacao1 > populacao2);
+  printf("Area: Carta %d venceu (%d)\n", (area1 > area2) ? 1 : 2, area1 > area2);
+  printf("PIB: Carta %d venceu (%d)\n", (pib1 > pib2) ? 1 : 2, pib1 > pib2);
+  printf("Pontos Turisticos: Carta %d venceu (%d)\n", (pontosTuristicos1 > pontosTuristicos2) ? 1 : 2, pontosTuristicos1 > pontosTuristicos2);
+  printf("Densidade Populacional: Carta %d venceu (%d)\n", (densidadePopulacional1 < densidadePopulacional2) ? 1 : 2, densidadePopulacional1 < densidadePopulacional2);
+  printf("PIB per Capita: Carta %d venceu (%d)\n", (pibPerCapita1 > pibPerCapita2) ? 1 : 2, pibPerCapita1 > pibPerCapita2);
+  printf("Super Poder: Carta %d venceu (%d)\n", (superPoder1 > superPoder2) ? 1 : 2, superPoder1 > superPoder2);
 
   printf("\n");
   system("pause");
